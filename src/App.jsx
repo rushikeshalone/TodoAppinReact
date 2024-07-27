8cc08e85defaa8256453d48218be5f2d5a1b8ff7
rushikeshalone <rushikeshalone.me@sbjit.edu.in>
2024-07-27 23:06:49 +0530

    Daily

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-4xl text-bold text-white bg-slate-900 p-5 rounded-xl ' 
+      > Rushikesh Daily Task</h1>
+      
+    </>
+  )
+}
+
+export default App
