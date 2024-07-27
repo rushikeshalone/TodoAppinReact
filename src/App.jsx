b5492c2379bc811611528725ce311bcbe2bbb10f
rushikeshalone <rushikeshalone.me@sbjit.edu.in>
2024-07-27 23:58:00 +0530

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +12,8 @@ function App() {
     <>
       <h1 className='text-4xl text-bold text-white bg-slate-900 p-5 rounded-xl ' 
       > Rushikesh Daily Task</h1>
-      
+      <AddTodo />
+      <Todos />
     </>
   )
 }
